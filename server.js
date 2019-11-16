f60b2a383a6c8706de16a72a0fea6c52de2844dd
asathiya007 <akshay.sathiya@gmail.com>
2019-11-15 22:01:03 -0800

    server created

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express(); 
+app.use(express.json());
+app.use(cors());
+
+// @route   GET /
+// @desc    wecreate endpoint
+// @access  public 
+app.get("/", (req, res) => {
+    res.json("WeCreate ready!");
+}); 
+
+// @route   GET /test
+// @desc    test api running endpoint
+// @access  public 
+app.get("/test", (req, res) => {
+    res.json("API running");
+}); 
+
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
